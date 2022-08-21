d5b3fba7e0e2c2c7fb4fcd24b6d2e146b3bde8ce
DANILO <jhondanilo3@gmail.com>
2022-08-21 00:05:18 +0000

    images changes

--- portafolio/src/Components/Hero/index.jsx
+++ portafolio/src/Components/Hero/index.jsx
@@ -1,35 +1,34 @@
-import React,{useEffect} from 'react'
+import React, { useEffect } from 'react'
 import './HeroElements.css';
 import Aos from "aos";
 import "aos/dist/aos.css"
+import imagen from '../images/Danilo.jpg'
 const Hero = () => {
 
 
-  
-        useEffect(()=>{
-            Aos.init({duration:2000});
-          },[]);
-    
-    return (
-        <div class="section"  data-aos="fade-rigth" id="inicio"> 
-       <div class="Contenedor" >
-         <header class="header" data-aos="fade-rigth">
-            <div clas="col">
-                <div class="content">
-                    <h1 class="heading" > About me<span>Danilo</span> </h1>
-                        <p clas="info">IM   STUDENT OF SYSTEM INGENIR IN THE UIS                       </p>
-                   
-                </div>
-            </div>
 
-            <div class="col">
-                <img>
-                </img>
+    useEffect(() => {
+        Aos.init({ duration: 2000 });
+    }, []);
+
+    return (
+        <div class="section" data-aos="fade-rigth" id="inicio">
+            <div class="Contenedor" >
+                <header class="header" data-aos="fade-rigth">
+                    <div clas="col">
+                        <div class="content">
+                            <h1 class="heading" > About me<span> DEVELOPER DANILO</span>  </h1>
+                            <p clas="info">IM   STUDENT OF SYSTEM INGENIR IN THE UIS                       </p>
+                        </div>
+
+                    </div>
+                    <div>
+                        <img src={imagen} class="svg" alt="" />
+                    </div>
+                </header>
             </div>
-        </header> 
         </div>
-        </div>
-     
+
     )
 }
 
